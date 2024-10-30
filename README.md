ce175f484d28cfd25abca0c1889c462e5acf4bfc
Joshua Liddell <joshua.liddell21@gmail.com>
2024-10-30 17:37:33 -0600

    General fixes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Mean-Inversion-Trader
+
+Takes CSV files containing 1yr historical stock price data and performs a mean inversion trading strategy to determine profitability. 
--- main.py
+++ main.py
@@ -2,38 +2,46 @@ import pandas as pd
 
 data = pd.read_csv('TSLAdata.csv')
 prices = data[['Close/Last']].rename(columns={'Close/Last': 'Price'})
-
 prices['5dayAvg'] = prices['Price'].rolling(window=5).mean()
 
-
-
-firstBuy = ''
-buyprice = ''
+firstBuy = None
+buyprice = None
 isbought = False
+totalprofit = 0
+profitAmts = [] 
 
-for row in prices.values:
-    price = row[0]
-    avg = row[1]
-    
-    
-    if firstBuy == '' and price < (avg*0.98):
+buyThreshold = 0.98
+sellThreshold = 1.02
+
+for index, row in prices.iterrows():
+    price = row['Price']
+    avg = row['5dayAvg']
+        
+    if firstBuy is None and price < (avg*buyThreshold):
         firstBuy = price
         print(f"Buying at: {price}")
         buyprice = price
         isbought = True
     
-    
-    elif price < (avg*0.98) and isbought == False:
+    elif price < (avg*buyThreshold) and isbought == False:
         print(f"Buying at: {price}")
         buyprice = price
         isbought = True
         
-    
-    elif isbought and price > (avg*1.02):
+    elif isbought and price > (avg*sellThreshold):
         print(f"Selling at: {price}")
-        print(f"Trade profit: {(buyprice-price)*-1}")
+        tprofit = round((price-buyprice), 2)
+        totalprofit += tprofit
+        print(f"Trade profit: {tprofit}")
         isbought = False
+        profitAmts.append(tprofit)
 
 
-    else:
-        print('Hold')
+avgt = sum(profitAmts)/len(profitAmts)
+print()
+print(f'------- {len(profitAmts)} Trades Complete -------')
+print(f'Total profit: {round(totalprofit,2)}')
+print(f'Average trade profit: {round(avgt,2)}')
+print(f'First buy: {firstBuy}')
+print(f'Percent return: {round((totalprofit/firstBuy)*100,2)}'+'%')
+
