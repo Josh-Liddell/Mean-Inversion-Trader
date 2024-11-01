eca9cb25efda13dc2b29432975d1a9b591b8c1f9
Joshua Liddell <joshua.liddell21@gmail.com>
2024-11-01 12:47:36 -0600

    Added multi file functionality

--- main.py
+++ main.py
@@ -1,47 +1,64 @@
 import pandas as pd
+import os
 
-data = pd.read_csv('TSLAdata.csv')
-prices = data[['Close/Last']].rename(columns={'Close/Last': 'Price'})
-prices['5dayAvg'] = prices['Price'].rolling(window=5).mean()
+folderpath = '/Users/joshua/Desktop/HW4-Trading/Data'
+bestprofit = 0 
 
-firstBuy = None
-buyprice = None
-isbought = False
-totalprofit = 0
-profitAmts = [] 
 
-buyThreshold = 0.98
-sellThreshold = 1.02
-
-for index, row in prices.iterrows():
-    price = row['Price']
-    avg = row['5dayAvg']
+for filename in os.listdir(folderpath):
+    if filename.endswith('.csv'):
+        filepath = os.path.join(folderpath, filename)  
+        data = pd.read_csv(filepath)  
         
-    if firstBuy is None and price < (avg*buyThreshold):
-        firstBuy = price
-        print(f"Buying at: {price}")
-        buyprice = price
-        isbought = True
-    
-    elif price < (avg*buyThreshold) and isbought == False:
-        print(f"Buying at: {price}")
-        buyprice = price
-        isbought = True
-        
-    elif isbought and price > (avg*sellThreshold):
-        print(f"Selling at: {price}")
-        tprofit = round((price-buyprice), 2)
-        totalprofit += tprofit
-        print(f"Trade profit: {tprofit}")
+        prices = data[['Close/Last']].rename(columns={'Close/Last': 'Price'})
+        prices['Price'] = prices['Price'].replace('[\$,]', '', regex=True).astype(float)
+        prices['5dayAvg'] = prices['Price'].rolling(window=5).mean()
+
+        firstBuy = None
+        buyprice = None
         isbought = False
-        profitAmts.append(tprofit)
+        totalprofit = 0
+        profitAmts = [] 
+
+        buyThreshold = 0.98
+        sellThreshold = 1.02
+
+        # Loops through each row and decides whether to buy, sell, otherwise it holds.
+        for index, row in prices.iterrows():
+            price = row['Price']
+            avg = row['5dayAvg']
+                
+            if firstBuy is None and price < (avg*buyThreshold):
+                firstBuy = price
+                # print(f"Buying at: {price}")
+                buyprice = price
+                isbought = True
+            
+            elif price < (avg*buyThreshold) and isbought == False:
+                # print(f"Buying at: {price}")
+                buyprice = price
+                isbought = True
+                
+            elif isbought and price > (avg*sellThreshold):
+                # print(f"Selling at: {price}")
+                tprofit = round((price-buyprice), 2)
+                totalprofit += tprofit
+                # print(f"Trade profit: {tprofit}")
+                isbought = False
+                profitAmts.append(tprofit)
+
+        avgt = sum(profitAmts)/len(profitAmts)
+        print()
+        print(f'------- {len(profitAmts)} Trades Complete for {filename[:-4]} -------')
+        print(f'Total profit: {round(totalprofit,2)}')
+        print(f'Average trade profit: {round(avgt,2)}')
+        print(f'First buy: {firstBuy}')
+        print(f'Percent return: {round((totalprofit/firstBuy)*100,2)}'+'%')
+
+        if totalprofit > bestprofit:
+            bestprofit = round(totalprofit,2)
+            fmessage = f"\nBest total profit was ${bestprofit} from {filename[:-4]}"
 
 
-avgt = sum(profitAmts)/len(profitAmts)
-print()
-print(f'------- {len(profitAmts)} Trades Complete -------')
-print(f'Total profit: {round(totalprofit,2)}')
-print(f'Average trade profit: {round(avgt,2)}')
-print(f'First buy: {firstBuy}')
-print(f'Percent return: {round((totalprofit/firstBuy)*100,2)}'+'%')
 
+print(fmessage)
