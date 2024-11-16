b4c36d9a4d5756a98fe39867cb0256ca7f554e4a
Joshua Liddell <joshua.liddell21@gmail.com>
2024-11-15 18:54:37 -0700

    Updated to ouput values to JSON

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Mean-Inversion-Trader
 
-The program takes CSV files containing 1yr historical stock price data and performs a mean inversion trading strategy to determine profitability. 
-
-It will loop through each price and it's corresponding 5 day average. If the price falls far enough below its 5 day average the program will buy and wait until the price rises far enough above the 5 day average at which point it will sell. 
+The program takes CSV files containing 1yr historical stock price data and performs a mean inversion trading strategy and a move average trading strategy to determine profitability. 
--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 import pandas as pd
 import os
+import json
 
-folderpath = '/Users/joshua/Desktop/HW4-Trading/Data'
-bestprofit = 0 
-mark = 1
+folderpath = os.path.join(os.getcwd(), 'Data')
 
-def meanReversionStrategy(filepath):
-    global bestprofit, mark
-    data = pd.read_csv(filepath)  
-    data = data.sort_values(by='Date', ascending=True, inplace=False)
-
-
-
-    prices = data[['Close/Last']].rename(columns={'Close/Last': 'Price'})
-    prices['Price'] = prices['Price'].replace('[\$,]', '', regex=True).astype(float)
-    prices['5dayAvg'] = prices['Price'].rolling(window=5).mean()
-
+def meanReversionStrategy(prices):
     firstBuy = None
     buyprice = None
     isbought = False
     totalprofit = 0
-    profitAmts = [] 
 
     buyThreshold = 0.98
     sellThreshold = 1.02
@@ -47,29 +35,80 @@ def meanReversionStrategy(filepath):
             totalprofit += tprofit
             # print(f"Trade profit: {tprofit}")
             isbought = False
-            profitAmts.append(tprofit)
 
-    avgt = sum(profitAmts)/len(profitAmts)
-    print()
-    print(f'------- {len(profitAmts)} Trades Complete for {filename[:-4]} -------')
-    print(f'Total profit: {round(totalprofit,2)}')
-    print(f'Average trade profit: {round(avgt,2)}')
-    print(f'First buy: {firstBuy}')
-    print(f'Percent return: {round((totalprofit/firstBuy)*100,2)}'+'%')
+    if isbought == True:
+        # print(f"Selling at: {price}")
+        tprofit = round((prices.iloc[-1]['Price']-buyprice), 2)
+        totalprofit += tprofit
+        # print(f"Trade profit: {tprofit}")
+        isbought = False
 
-    if totalprofit > bestprofit or mark == 1:
-        bestprofit = round(totalprofit,2)
-        global fmessage
-        fmessage = f"\nBest total profit was ${bestprofit} from {filename[:-4]}"
-
-    mark += 1
+    return round(totalprofit,2), round((totalprofit/firstBuy)*100,2)
 
 
+def simpleMovingAverageStrategy(prices):
+    firstBuy = None
+    buyprice = None
+    isbought = False
+    totalprofit = 0
+
+
+    for index, row in prices.iterrows():
+        price = row['Price']
+        avg = row['5dayAvg']
+            
+        if firstBuy is None and price < avg:
+            firstBuy = price
+            # print(f"Buying at: {price}")
+            buyprice = price
+            isbought = True
+        
+        elif isbought == False and price < avg:
+            # print(f"Buying at: {price}")
+            buyprice = price
+            isbought = True
+            
+        elif isbought and price > avg:
+            # print(f"Selling at: {price}")
+            tprofit = round((price-buyprice), 2)
+            totalprofit += tprofit
+            # print(f"Trade profit: {tprofit}")
+            isbought = False
+        
+    if isbought == True:
+        # print(f"Selling at: {price}")
+        tprofit = round((prices.iloc[-1]['Price']-buyprice), 2)
+        totalprofit += tprofit
+        # print(f"Trade profit: {tprofit}")
+        isbought = False
+    
+    return round(totalprofit,2), round((totalprofit/firstBuy)*100,2)
+
+
+dataDict = {}
 
 for filename in os.listdir(folderpath):
     if filename.endswith('.csv'):
+        ticker = filename[:-4]
         filepath = os.path.join(folderpath, filename)  
-        meanReversionStrategy(filepath)
+        data = pd.read_csv(filepath)  
+        data = data.sort_values(by='Date', ascending=True, inplace=False)
+        prices = data[['Close/Last']].rename(columns={'Close/Last': 'Price'})
+        prices['Price'] = prices['Price'].replace('[\$,]', '', regex=True).astype(float)
+        prices['5dayAvg'] = prices['Price'].rolling(window=5).mean()
 
 
-print(fmessage)
+        totalprofit, preturn = simpleMovingAverageStrategy(prices)
+        mtotalprofit, mpreturn = meanReversionStrategy(prices)
+
+        plist = prices['Price'].tolist()
+        dataDict[filename[:-4]+'_prices'] = plist
+        dataDict[ticker+'_sma_profit'] = totalprofit
+        dataDict[ticker+'_sma_returns'] = preturn
+        dataDict[ticker+'_mr_profit'] = mtotalprofit
+        dataDict[ticker+'_mr_returns'] = mpreturn
+
+
+json.dump(dataDict, open("tradeData.json", "w"), indent=4)
+
+print("Process Complete")
